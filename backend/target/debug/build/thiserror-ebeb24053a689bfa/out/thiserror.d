2f65007c6f6d63db072b98d5c3df47f6bb219015
killuayz <2959243019@qq.com>
2024-08-29 18:20:09 +0800

    删除多余文件

--- backend/target/debug/build/thiserror-ebeb24053a689bfa/out/thiserror.d
+++
@@ -1,7 +0,0 @@
-C:\Users\ziyang\Documents\PondRoots\backend\target\debug\build\thiserror-ebeb24053a689bfa\out\libthiserror.rmeta: build\probe.rs
-
-C:\Users\ziyang\Documents\PondRoots\backend\target\debug\build\thiserror-ebeb24053a689bfa\out\thiserror.d: build\probe.rs
-
-build\probe.rs:
-
-# env-dep:RUSTC_BOOTSTRAP
--- backend/target/debug/deps/pond_roots.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ziyang\Documents\PondRoots\backend\target\debug\deps\pond_roots.exe: src/main.rs
-
-C:\Users\ziyang\Documents\PondRoots\backend\target\debug\deps\pond_roots.d: src/main.rs
-
-src/main.rs:
